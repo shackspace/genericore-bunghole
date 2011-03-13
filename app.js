d11155ebe527cd109e0c55b084290c428603fccd
Sven Pfleiderer <sg+github@roothausen.de>
2011-03-13 23:08:22 +0100

    Add socket.io and log4js support

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
-/**
- * Module dependencies.
+/*!
+ * app.js
+ *
+ * @author pfleidi
  */
 
-var Express = require('express');
+var Fs = require('fs');
 var Util = require('util');
+var Express = require('express');
+var Io = require('socket.io');
+var Log4js = require('log4js')();
+var logger = Log4js.getLogger('bunghole');
+var LOGFILE = __dirname + '/logs/bunghole.log';
+
+// main application
 var app = module.exports = Express.createServer();
 
-// Configuration
+/**
+ * configuration
+ */
 app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
     app.use(Express.bodyParser());
-    app.use(Express.methodOverride());
     app.use(app.router);
     app.use(Express.static(__dirname + '/public'));
   });
 
 app.configure('development', function () {
-    app.use(Express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(Express.errorHandler({
+          dumpExceptions: true,
+          showStack: true
+        }));
+    logger.setLevel('DEBUG');
   });
 
 app.configure('production', function () {
-    app.use(Express.errorHandler()); 
+    var accessLog = Fs.createWriteStream(__dirname + '/logs/access.log', {
+        encoding: 'utf-8',
+        flags: 'a'
+      });
+
+    app.use(Express.logger({ stream: accessLog }));
+    app.use(Express.errorHandler());
+    Log4js.addAppender(Log4js.fileAppender(LOGFILE));
+    logger.setLevel('ERROR');
   });
 
-// Only listen on $ node app.js
-if (!module.parent) {
-  app.listen(3000, function () {
-      var address = app.address();
-      Util.puts('Express server listening on http://' + address.address + ':' + address.port + '/');
-    });
-}
+
+app.listen(3000, function () {
+    var adr = app.address();
+    Util.puts('Express server listening on http://' + adr.address + ':' + adr.port + '/');
+  });
+
+var webSocketServer = Io.listen(app, {
+    flashPolicyServer: false
+  });
+
+webSocketServer.on('connection', function (connection) {
+
+    function _dispatch(msg, connection) {
+      console.dir(msg);
+    }
+
+    connection.on('message', function (message) {
+        try {
+          var msg = JSON.parse(message);
+          _dispatch(msg, connection);
+        } catch (err) {
+          log.error('Couldn\'t parse or eval message: ' + err.stack);
+        }
+      });
+  });
+
+/* Process Logging */
+
+process.on('SIGINT', function () {
+    logger.info('Got SIGINT. Exiting ...');
+    process.exit(0);
+  });
+
+process.on('uncaughtException', function (err) {
+    logger.fatal('RUNTIME ERROR! :' + err.stack);
+  });
--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     var i = 0;
 
     function update() {
-      i += 0.2;
+      i += 0.1;
       d1.push([i, Math.sin(i)]);
       $.plot($("#container"), [ d1 ]);
-      setTimeout(update, 200);
+      setTimeout(update, 100);
     }
     
     update();
